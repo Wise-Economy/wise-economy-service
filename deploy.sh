4e179c6c3655ebce14b99bbcd66838c67101bac9
sarat chandra <saratchandra@MacBook-Pro.Dlink>
2020-07-26 15:59:15 +0530

    added deploy scritps

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+#update packages
+apt-get update -y
+apt-get install -y gettext-base
+
+#build the latest code changes.
+mvn clean install -DskipTests -DoutputDirectory=/home/ubuntu/wise_economy/service/wise-economy-service/latest
+
+#add the service file to systemctl.
+sudo cp /home/ubuntu/wise_economy/service/wise-economy-service/wise-economy.service /etc/systemd/system/
+
+# Update the system-daemon
+sudo systemctl daemon-reload
+
+# stop the existing process
+sudo service wise-economy restart
+
