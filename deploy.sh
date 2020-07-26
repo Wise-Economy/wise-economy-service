573a494df1942f9a46b9935c443e977cf6d6aa44
sarat chandra <saratchandra@MacBook-Pro.Dlink>
2020-07-26 16:42:33 +0530

    added correct jar folder

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ apt-get update -y
 apt-get install -y gettext-base
 
 #build the latest code changes.
-mvn clean install -DskipTests -DoutputDirectory=/home/ubuntu/wise_economy/service/wise-economy-service/latest
+mvn clean install -DskipTests -DoutputDirectory=/home/ubuntu/wise_economy/service/wise-economy-service/target
 
 #add the service file to systemctl.
 sudo cp /home/ubuntu/wise_economy/service/wise-economy-service/wise-economy.service /etc/systemd/system/
